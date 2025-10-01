a0a4d976129a035c1dc02615d8cfd837a0e38536
Yurii-Nedzelnytskyi <kerkskir2@gmail.com>
2025-10-01 20:28:23 +0300

    Add login availability check during user registration

--- sign-up.js
+++ sign-up.js
@@ -13,6 +13,11 @@ function loadUsers() {
 function handleSubmit() {
   const user = Object.fromEntries(new FormData(form));
 
+  if (isOccupied(user.login)) {
+    alert('This login is already taken');
+    return false;
+  }
+
   if (user.password != user.password2) {
     alert('Passwords do not match');
     return false;
@@ -20,6 +25,15 @@ function handleSubmit() {
 
   register(user);
   saveUsers();
+}
+
+function isOccupied(login) {
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].login === login) {
+      return true;
+    }
+  } 
+  return false;
 }
 
 function register(user) {
